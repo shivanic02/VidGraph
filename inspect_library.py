7f63300d27d5bc9ba4692d5a1fa773e079816c27
Shivani Chauhan <85115639+shivanic02@users.noreply.github.com>
2025-11-28 22:08:19 +0000

    inspect_library.py

---
+++ inspect_library.py
@@ -0,0 +1,7 @@
+from youtube_transcript_api import YouTubeTranscriptApi
+
+print("--- INSPECTION START ---")
+print(f"Object Location: {YouTubeTranscriptApi}")
+print("Attributes available:")
+print(dir(YouTubeTranscriptApi))
+print("--- INSPECTION END ---")
